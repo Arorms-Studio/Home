79b8b414074b2e597988c73696c6c25690fdf032
Cacciatore <holmesamzish86@outlook.com>
2025-08-06 17:27:15 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+function App() {
+    return (
+        <div className="flex flex-col justify-center items-center">
+
+            <div
+                className="bg-[url('/bg-light-abstract.jpg')] bg-cover bg-center
+                w-3/5 h-96 rounded-xl shadow-xl flex-col justify-center items-center mt-16"
+            >
+
+                <h1 className="text-center text-3xl font-bold text-white p-8 mt-8">Arorms Studio</h1>
+                <p className="text-xl font-semibold text-white p-8">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
+                    animi ex fugiat id illum iste officiis sapiente veniam vero
+                    voluptate? Accusamus ad architecto dolor dolorem iure placeat
+                    voluptatum. Impedit, quos.
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default App;
